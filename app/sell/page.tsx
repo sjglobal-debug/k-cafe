4603ce3368833f1b157ae60bc789a41641ae1b7e
sjglobal-debug <sjglobal@sjglobal.info>
2026-04-14 01:22:08 +0900

    Update page.tsx

--- app/sell/page.tsx
+++ app/sell/page.tsx
@@ -1,1 +1,8 @@
-
+export default function Home() {
+  return (
+    <div style={{ padding: 40 }}>
+      <h1>SJ GLOBAL K-CAFE PLATFORM</h1>
+      <a href="/sell">판매 등록 바로가기</a>
+    </div>
+  );
+}
