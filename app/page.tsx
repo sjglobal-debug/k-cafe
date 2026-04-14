c3dedadd57a184fc2d3da6acefb6876a013163aa
sjglobal-debug <sjglobal@sjglobal.info>
2026-04-14 19:44:01 +0900

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,40 @@
-export default function Home() {
-  return <h1>홈 화면</h1>;
+"use client";
+
+import { useState } from "react";
+import { db } from "../firebase";
+import { collection, addDoc } from "firebase/firestore";
+
+export default function SellPage() {
+  const [title, setTitle] = useState("");
+  const [brand, setBrand] = useState("");
+
+  const handleSubmit = async () => {
+    await addDoc(collection(db, "products"), {
+      title,
+      brand,
+      createdAt: new Date(),
+    });
+
+    alert("등록 완료!");
+  };
+
+  return (
+    <div style={{ padding: 40 }}>
+      <h1>판매 등록 테스트</h1>
+
+      <input
+        placeholder="제목"
+        value={title}
+        onChange={(e) => setTitle(e.target.value)}
+      />
+
+      <input
+        placeholder="브랜드"
+        value={brand}
+        onChange={(e) => setBrand(e.target.value)}
+      />
+
+      <button onClick={handleSubmit}>등록</button>
+    </div>
+  );
 }
